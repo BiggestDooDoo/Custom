e7782b8c55ac797dc70a8f95c40620c90aa195e6
BiggestDooDoo <46853593+BiggestDooDoo@users.noreply.github.com>
2019-01-20 01:13:30 +0200

    Create How2Run.js

---
+++ How2Run.js
@@ -0,0 +1,21 @@
+const discord = require ('discord.js');
+var client = new discord.Client();
+
+client.on ("ready", () => {
+    console.log ("readeh");
+
+    client.user.setGame ("DM YMA_ST1#0021 for help!");
+});
+
+const prefix = "R!"
+
+client.on ("message", (message) => {
+    if (message.author.bot) return;
+
+    if (message.content.startsWith (prefix)) {
+        message.react('✅');
+        message.react('❎');
+    }
+});
+
+client.login (process.env.BOT_TOKEN);
