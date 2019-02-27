a37b56401abb18a8851d510ab18fa0df9e0ffcc2
BiggestDooDoo <46853593+BiggestDooDoo@users.noreply.github.com>
2019-02-27 18:28:57 +0200

    Update How2Run.js

--- How2Run.js
+++ How2Run.js
@@ -4,20 +4,16 @@ var client = new discord.Client();
 client.on ("ready", () => {
     console.log ("readeh");
 
-    client.user.setGame ("DM YMA_ST1#0021 for help!");
+    client.user.setGame ("DM YMA_ST1#5688 for help!");
 });
-
-const prefix = "R!"
-const prefix2 = "r!"
 
 client.on ("message", (message) => {
     
     if (message.author.bot) return;
 
-    if (message.content.startsWith ("")) {
-        message.react('✅');
-        message.react('❎');
-    }
+    message.react('✅');
+    message.react('❎');
+    
 });
 
 client.login (process.env.BOT_TOKEN);
